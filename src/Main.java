3df903bf979693df5e7bf2fc2c791acd99923bf7
Carlos Barrera <74219724+barrera-max@users.noreply.github.com>
2024-05-21 12:57:46 -0300

    Delete src directory

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-package com.BonVino.proyect;
-
-public class Main {
-    public static void main(String[] args) {
-        PantallaAdminActualizaciones.launch(PantallaAdminActualizaciones.class, args);
-    }
-}
